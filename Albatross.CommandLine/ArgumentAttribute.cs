a87b38af3fe7556e37c8652ca7cdc3c09a92d2a5
rushuiguan <rushui@outlook.com>
2025-03-24 13:40:22 -0400

    change the property type of ArgumentAttribute.ArityMin and ArgumentAttribute.ArityMax from int? to int

--- Albatross.CommandLine/ArgumentAttribute.cs
+++ Albatross.CommandLine/ArgumentAttribute.cs
@@ -4,8 +4,8 @@ namespace Albatross.CommandLine {
 	public class ArgumentAttribute : Attribute {
 		public string? Description { get; set; }
 		public bool Hidden { get; set; }
-		public int? ArityMin { get; set; }
-		public int? ArityMax { get; set; }
+		public int ArityMin { get; set; }
+		public int ArityMax { get; set; }
 		public int Order { get; set; }
 		/// <summary>
 		/// When true, the code generator will generate a default value based on the initializer of the property
