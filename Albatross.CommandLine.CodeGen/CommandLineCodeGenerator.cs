88d76bfb64555c65154c1d4a7f5f5ac3ff463181
rushuiguan <rushui@gmail.com>
2025-12-24 00:09:30 -0500

    update to latest codegen

--- Albatross.CommandLine.CodeGen/CommandLineCodeGenerator.cs
+++ Albatross.CommandLine.CodeGen/CommandLineCodeGenerator.cs
@@ -68,7 +68,7 @@ namespace Albatross.CommandLine.CodeGen {
 			context.RegisterSourceOutput(
 				aggregated,
 				static (context, data) => {
-					var typeConverter = new DefaultTypeConverter(data.compilation);
+					var typeConverter = new DefaultTypeConverter();
 					var (compilation, commandSetups) = data;
 					var builder = new CommandClassBuilder(compilation, typeConverter);
 					foreach (var group in commandSetups.GroupBy(x => x.CommandClassName)) {
