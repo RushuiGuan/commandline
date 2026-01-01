7db0a21bb907f5052c946216c706574bce1533ca
Rushui Guan <rushui@outlook.com>
2026-01-01 10:26:57 -0500

    add Inputs project

---
+++ Albatross.CommandLine.Inputs/FormatExpression.cs
@@ -0,0 +1,28 @@
+using Albatross.CommandLine.Annotations;
+using Albatross.Expression.Parsing;
+using System.CommandLine;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Albatross.CommandLine.BuiltIn {
+	[DefaultOptionHandler(typeof(ParseFormatExpression))]
+	public class FormatExpressionOption : Option<string>, IUseContextValue{
+		public FormatExpressionOption(string name, params string[] aliases) : base(name, aliases) {
+		}
+		public FormatExpressionOption() : this("--format", "-f") {
+		}
+	}
+	public class ParseFormatExpression: IAsyncOptionHandler<FormatExpressionOption> {
+		private readonly ICommandContext context;
+		public ParseFormatExpression(ICommandContext context) {
+			this.context = context;
+		}
+		public Task InvokeAsync(FormatExpressionOption symbol, ParseResult result, CancellationToken cancellationToken) {
+			var text = result.GetValue(symbol);
+			if (!string.IsNullOrEmpty(text)) {
+				var parser = Albatross.Text.CliFormat.Extensions.BuildCustomParser();
+				var tree = parser.Build(text);
+			}
+		}
+	}
+}
