830dc8d3eb655573fb82eb3a52adda626bcb4854
rushuiguan <rushui@outlook.com>
2026-01-08 12:58:00 -0500

    fix sample code

---
+++ Sample.CommandLine/TestAliasNameConflict.cs
@@ -0,0 +1,16 @@
+﻿using Albatross.CommandLine;
+using Albatross.CommandLine.Annotations;
+
+namespace Sample.CommandLine {
+	[Verb<DefaultAsyncCommandHandler<TestAliasConflictParams>>("test alias-conflict")]
+	public class TestAliasConflictParams {
+		[Option("n", "n1")]
+		public string? Name1 { get; set; }
+
+		[Option("n", "n2")]
+		public string? Name2 { get; set; }
+
+		[Option("--name1", "n3")]
+		public string? Name3 { get; set; }
+	}
+}
